449d87ef1b35c78daf83234e0436a224452cf8e3
changliao1025 <changliao1025@gmail.com>
2020-03-21 20:31:20 -0600

    initial

---
+++ src/watershed.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file watershed.cpp
+ * @author Chang Liao (chang.liao@pnnl.gov)
+ * @brief 
+ * @version 0.1
+ * @date 2019-08-02
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#include "watershed.h"
+namespace hexwatershed
+    {
+
+    }
---
+++ src/watershed.h
@@ -0,0 +1,27 @@
+/**
+ * @file watershed.h
+ * @author Chang Liao (chang.liao@pnnl.gov)
+ * @brief 
+ * @version 0.1
+ * @date 2019-08-02
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+
+
+#include <vector>
+#include "hexagon.h"
+using namespace std;
+
+namespace hexwatershed
+    {
+        class watershed
+            {
+
+                std::vector<hexagon> vCell;
+            };
+    }
+
+
