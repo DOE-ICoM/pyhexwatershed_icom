114f4056fe5bb9ee126ee0cf80d3e8f7e9cf19d3
changliao1025 <changliao1025@outlook.com>
2022-06-07 20:09:29 -0700

    testing job file

--- README.md
+++ README.md
@@ -1,67 +1,18 @@
 # Introduction
-This a repository for the HexWatershed model on the MPAS mesh only.
+
+This a repository for the HexWatershed part 2 study.
 For more details, please refer to the HexWatershed documentation (https://hexwatershed.readthedocs.io/).
 
 # Requirements
 
-You need internet connection and several tools to run the examples in the tutorial.
+You need to install both the C++ backend and Python frontend of the HexWatershed model.
 
-- conda 
-- cmake
-- c++ compiler
-- QGIS (optional)
+# Model configuration
 
-You need addition tools (e.g., QGIS) to visulize some of the model results.
-
-# Step-by-step instruction
-
-
-1. Download additional data files using a internet brower (Chrome recommended)
-
-Download the dem1.tif and lnd_cull_mesh.nc files from
-
-https://rcdemo.pnnl.gov/workshop/
-
-2. Install the HexWatershed C++ component
-
-- `git clone https://github.com/changliao1025/hexwatershed.git`
-
-- `cd hexwatershed/build`
-
-- `cmake CMakeLists.txt  -DCMAKE_CXX_COMPILER=g++-10`
-
-- `make install`
-
-3. Create/activate a conda environment and install Python packages
-
-- `conda create --name hexwatershed_tutorial python=3.8`
-
-- `conda install -c conda-forge pyflowline`
-
-- `pip install hexwatershed`
-
-Because the `GDAL` library is used by this project and the `proj` library is often not configured correctly automatically. 
-
-On Linux or Mac, you can set it up like this, `.bash_profile` as an example:
-
-Anaconda:
-
-`export PROJ_LIB=/people/user/.conda/envs/hexwatershed_tutorial/share/proj`
-
-
-Miniconda:
-
-`export PROJ_LIB=/opt/miniconda3/envs/hexwatershed_tutorial/share/proj`
-
-4. Download this tutorial
-
-`git clone https://github.com/changliao1025/hexwatershed_tutorial.git`
-
-5. Run the examples within the `example` folder
-
-- You need to edit the template configuration file to match with your data set.
-
-- Depending where you downloaded the data and the example, different configurations are required.
+| Syntax      | Description |
+| ----------- | ----------- |
+| Header      | Title       |
+| Paragraph   | Text        |
 
 
 # References
--- example/susquehanna/example_susquehanna.py
+++ example/susquehanna/example_susquehanna.py
@@ -1,5 +1,6 @@
 
 import os, sys
+from sre_constants import IN
 from pathlib import Path
 from os.path import realpath
 import argparse
@@ -12,47 +13,54 @@ logging.warning('is the time pyhexwatershed simulation started.')
 
 from pyhexwatershed.classes.pycase import hexwatershedcase
 from pyhexwatershed.pyhexwatershed_read_model_configuration_file import pyhexwatershed_read_model_configuration_file
-from pyhexwatershed.pyhexwatershed_generate_template_configuration_file import pyhexwatershed_generate_template_configuration_file
+
 
 sMesh_type = 'mpas'
 iCase_index = 1
 dResolution_meter=5000
-sDate='20220517'
+sDate='20220607'
 sPath = str( Path().resolve() )
 iFlag_option = 1
 sWorkspace_data = realpath( sPath +  '/data/susquehanna' )
 sWorkspace_input =  str(Path(sWorkspace_data)  /  'input')
-sWorkspace_output=  str(Path(sWorkspace_data)  /  'output')
+sWorkspace_output=  '/compyfs/liao313/04model/pyhexwatershed/susquehanna'
 
 
-sFilename_configuration_in = realpath( sPath +  '/tests/configurations/pyhexwatershed_susquehanna_mpas.json' )
-        
-if os.path.isfile(sFilename_configuration_in):
-    print(sFilename_configuration_in)
-else:
-    print('This shapefile does not exist: ', sFilename_configuration_in )
-    exit()
-oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
-    iCase_index_in=iCase_index, sDate_in= sDate, sMesh_type_in= sMesh_type)     
+aMesh_type = ['hexagon', 'square','latlon','mpas']
+aResolution_meter = [5000, 40000]
+iCase_index = 1
+for iMesh_type in range(1, 5):
+    sMesh_type = aMesh_type[iMesh_type-1]
+    if sMesh_type=='hexagon':
+        sFilename_configuration_in = realpath( sPath +  '/example/susquehanna/pyhexwatershed_susquehanna_hexagon.json' )
+    else:
+        if sMesh_type=='square':
+            sFilename_configuration_in = realpath( sPath +  '/example/susquehanna/pyhexwatershed_susquehanna_square.json' )
+        else:
+            if sMesh_type=='latlon':
+                sFilename_configuration_in = realpath( sPath +  '/example/susquehanna/pyhexwatershed_susquehanna_latlon.json' )
+            else:
+                sFilename_configuration_in = realpath( sPath +  '/example/susquehanna/pyhexwatershed_susquehanna_mpas.json' )
 
-print(oPyhexwatershed.tojson())
-
-
-oPyhexwatershed.pPyFlowline.aBasin[0].dLatitude_outlet_degree=39.4620
-oPyhexwatershed.pPyFlowline.aBasin[0].dLongitude_outlet_degree=-76.0093
-oPyhexwatershed.setup()
-oPyhexwatershed.pPyFlowline.dLongitude_left= -79
-oPyhexwatershed.pPyFlowline.dLongitude_right= -74.5
-oPyhexwatershed.pPyFlowline.dLatitude_bot= 39.20
-oPyhexwatershed.pPyFlowline.dLatitude_top= 42.8
-oPyhexwatershed.run_pyflowline()
-oPyhexwatershed.pPyFlowline.export()
-oPyhexwatershed.export_config_to_json()
-oPyhexwatershed.run_hexwatershed()
-oPyhexwatershed.analyze()
-oPyhexwatershed.export()
-      
-
-print('Finished')
-logging.basicConfig(format='%(asctime)s %(message)s')
-logging.warning('is the time pyhexwatershed simulation finished.')
+    
+    if os.path.isfile(sFilename_configuration_in):
+        print(sFilename_configuration_in)
+    else:
+        print('This configuration file does not exist: ', sFilename_configuration_in )
+        exit()
+    if iMesh_type != 4:
+        for iResolution in range(1, 3):    
+            dResolution_meter = aResolution_meter[iResolution-1]
+            oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
+                iCase_index_in=iCase_index, dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
+            oPyhexwatershed.create_hpc_job()
+            iCase_index = iCase_index + 1
+            continue            
+            
+    else:
+        #mpas mesh only has one resolution
+        oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
+                iCase_index_in=iCase_index, dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
+        oPyhexwatershed.create_hpc_job()
+        iCase_index = iCase_index + 1
+        pass
