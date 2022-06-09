27f55af1d304d39d587b2c4256ac6bdae450b835
changliao1025 <changliao1025@outlook.com>
2022-06-09 14:44:07 -0700

    add bash to submit all cases

--- example/susquehanna/example_susquehanna.py
+++ example/susquehanna/example_susquehanna.py
@@ -1,5 +1,5 @@
 
-import os, sys
+import os, sys, stat
 from sre_constants import IN
 from pathlib import Path
 from os.path import realpath
@@ -29,6 +29,13 @@ sWorkspace_output=  '/compyfs/liao313/04model/pyhexwatershed/susquehanna'
 aMesh_type = ['hexagon', 'square','latlon','mpas']
 aResolution_meter = [5000, 40000]
 iCase_index = 1
+
+#generate a bash job script
+
+sFilename = sWorkspace_output + '/' + sDate  + 'submit.bash'
+ofs = open(sFilename, 'w')
+sLine  = '#!/bin/bash' + '\n'
+ofs.write(sLine)
 for iMesh_type in range(1, 5):
     sMesh_type = aMesh_type[iMesh_type-1]
     if sMesh_type=='hexagon':
@@ -52,39 +59,80 @@ for iMesh_type in range(1, 5):
         for iResolution in range(1, 3):    
             iFlag_stream_burning_topology = 0 
             dResolution_meter = aResolution_meter[iResolution-1]
+            iFlag_stream_burning_topology = 0
+            iFlag_use_mesh_dem = 0
+            iFlag_elevation_profile = 0
             oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
-                iCase_index_in=iCase_index,iFlag_stream_burning_topology_in=iFlag_stream_burning_topology,\
+                iCase_index_in=iCase_index,\
+                    iFlag_stream_burning_topology_in=iFlag_stream_burning_topology,\
+                        iFlag_use_mesh_dem_in=iFlag_use_mesh_dem,\
+                            iFlag_elevation_profile_in=iFlag_elevation_profile,\
                      dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
             oPyhexwatershed.create_hpc_job()
             oPyhexwatershed.export_config_to_json()
             print(iCase_index)
+
+            sLine  = 'cd ' + oPyhexwatershed.sWorkspace_output + '\n'
+            ofs.write(sLine)
+            sLine  = 'sbatch submit.job' + '\n'
+            ofs.write(sLine)
+
             iCase_index = iCase_index + 1
+
             iFlag_stream_burning_topology = 1
             oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
                 iCase_index_in=iCase_index,  iFlag_stream_burning_topology_in=iFlag_stream_burning_topology,\
+                      iFlag_use_mesh_dem_in=iFlag_use_mesh_dem,\
+                            iFlag_elevation_profile_in=iFlag_elevation_profile,\
                     dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
             oPyhexwatershed.create_hpc_job()
             oPyhexwatershed.export_config_to_json()
             print(iCase_index)
+
+            sLine  = 'cd ' + oPyhexwatershed.sWorkspace_output + '\n'
+            ofs.write(sLine)
+            sLine  = 'sbatch submit.job' + '\n'
+            ofs.write(sLine)
+
             iCase_index = iCase_index + 1
+
             continue            
             
     else:
         #mpas mesh only has one resolution
         iFlag_stream_burning_topology = 0 
+        iFlag_use_mesh_dem = 1
+        iFlag_elevation_profile = 1
         oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
                 iCase_index_in=iCase_index,iFlag_stream_burning_topology_in=iFlag_stream_burning_topology,\
+                      iFlag_use_mesh_dem_in=iFlag_use_mesh_dem,\
+                            iFlag_elevation_profile_in=iFlag_elevation_profile,\
                      dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
         oPyhexwatershed.create_hpc_job()
         print(iCase_index)
+        sLine  = 'cd ' + oPyhexwatershed.sWorkspace_output + '\n'
+        ofs.write(sLine)
+        sLine  = 'sbatch submit.job' + '\n'
+        ofs.write(sLine)
         iCase_index = iCase_index + 1
 
         iFlag_stream_burning_topology = 1 
         oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
                 iCase_index_in=iCase_index,iFlag_stream_burning_topology_in=iFlag_stream_burning_topology,\
+                      iFlag_use_mesh_dem_in=iFlag_use_mesh_dem,\
+                            iFlag_elevation_profile_in=iFlag_elevation_profile,\
                      dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
         oPyhexwatershed.create_hpc_job()
         oPyhexwatershed.export_config_to_json()
+
+        sLine  = 'cd ' + oPyhexwatershed.sWorkspace_output + '\n'
+        ofs.write(sLine)
+        sLine  = 'sbatch submit.job' + '\n'
+        ofs.write(sLine)
+
         print(iCase_index)
         iCase_index = iCase_index + 1
         pass
+
+ofs.close()
+os.chmod(sFilename, stat.S_IREAD | stat.S_IWRITE | stat.S_IXUSR)   
