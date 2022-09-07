b3f55b88db996a4e1534c4ca98352e02edbc1869
changliao1025 <changliao1025@outlook.com>
2022-09-06 20:56:22 -0700

    update workflow for example

---
+++ examples/susquehanna/run_simulation_hexagon.py
@@ -0,0 +1,126 @@
+
+import os, sys, stat
+from pathlib import Path
+from os.path import realpath
+
+from pyhexwatershed.pyhexwatershed_read_model_configuration_file import pyhexwatershed_read_model_configuration_file
+
+#===========================
+#setup workspace path
+#===========================
+sPath_parent = str(Path(__file__).parents[2]) # data is located two dir's up
+sPath_data = realpath( sPath_parent +  '/data/susquehanna' )
+sWorkspace_input =  str(Path(sPath_data)  /  'input')
+sWorkspace_output = '/compyfs/liao313/04model/pyhexwatershed/susquehanna'
+
+#===================================
+#you need to update this file based on your own case study
+#===================================
+sFilename_configuration_in = realpath( sPath_parent +  '/examples/susquehanna/pyhexwatershed_susquehanna_hexagon.json' )
+if os.path.isfile(sFilename_configuration_in):
+    pass
+else:
+    print('This configuration does not exist: ', sFilename_configuration_in )
+
+
+#===========================
+#setup case information
+#===========================
+iFlag_visualization =0 
+iCase_index = 1
+aResolution_meter = [5000, 40000]
+nResolution = len(aResolution_meter)
+sMesh_type = 'hexagon'
+sDate='20220801'
+
+#===================================
+#setup output and HPC job 
+#===================================
+sSlurm = 'short'
+sFilename = sWorkspace_output + '/' + sMesh_type + '.bash'
+ofs = open(sFilename, 'w')
+sLine  = '#!/bin/bash' + '\n'
+ofs.write(sLine)
+
+#===================================
+#visualization spatial extent
+#===================================
+aExtent_full = [-78.5,-75.5, 39.2,42.5]
+aExtent_meander = [-76.5,-76.2, 41.6,41.9] #meander
+aExtent_braided = [-77.3,-76.5, 40.2,41.0] #braided
+aExtent_confluence = [-77.3,-76.5, 40.2,41.0] #confluence
+aExtent_outlet = [-76.0,-76.5, 39.5,40.0] #outlet
+aExtent_dam = [-75.75,-76.15, 42.1,42.5] #dam  
+
+for iResolution in range(1, nResolution):        
+    dResolution_meter = aResolution_meter[iResolution-1]
+
+    #===================================
+    #topology turn off
+    #===================================
+    iFlag_stream_burning_topology = 0
+    iFlag_use_mesh_dem = 0
+    iFlag_elevation_profile = 0
+    oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
+        iCase_index_in=iCase_index,\
+            iFlag_stream_burning_topology_in=iFlag_stream_burning_topology,\
+                iFlag_use_mesh_dem_in=iFlag_use_mesh_dem,\
+                    iFlag_elevation_profile_in=iFlag_elevation_profile,\
+             dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
+    oPyhexwatershed.create_hpc_job()            
+    
+    sLine  = 'cd ' + oPyhexwatershed.sWorkspace_output + '\n'
+    ofs.write(sLine)
+    sLine  = 'sbatch submit.job' + '\n'
+    ofs.write(sLine)
+    if iFlag_visualization == 1:
+        oPyhexwatershed._plot(iFlag_type_in =1, sVariable_in = 'elevation')             
+        oPyhexwatershed._plot(iFlag_type_in =1, sVariable_in = 'slope') 
+        oPyhexwatershed._plot(iFlag_type_in =1, sVariable_in = 'drainagearea')   
+        oPyhexwatershed._plot(iFlag_type_in =2, sVariable_in = 'flow_direction') 
+        oPyhexwatershed._plot(iFlag_type_in =3, sVariable_in = 'flow_direction')    
+        oPyhexwatershed._plot(iFlag_type_in =4, sVariable_in = 'flow_direction')   
+
+    iCase_index = iCase_index + 1
+
+    #===================================
+    #topology turn on
+    #===================================
+    iFlag_stream_burning_topology = 1
+    oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
+        iCase_index_in=iCase_index,  iFlag_stream_burning_topology_in=iFlag_stream_burning_topology,\
+              iFlag_use_mesh_dem_in=iFlag_use_mesh_dem,\
+                    iFlag_elevation_profile_in=iFlag_elevation_profile,\
+            dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
+    
+    oPyhexwatershed.create_hpc_job()      
+    if iFlag_visualization == 1:
+        sFilename = os.path.join(  oPyhexwatershed.sWorkspace_output_hexwatershed, 'surface_elevation.png' )
+        oPyhexwatershed._plot(sFilename, iFlag_type_in =1, sVariable_in = 'elevation')     
+
+        sFilename = os.path.join(  oPyhexwatershed.sWorkspace_output_hexwatershed, 'surface_slope.png' )        
+        oPyhexwatershed._plot(iFlag_type_in =1, sVariable_in = 'slope') 
+
+        sFilename = os.path.join(  oPyhexwatershed.sWorkspace_output_hexwatershed, 'surface_elevation' )
+        oPyhexwatershed._plot(iFlag_type_in =1, sVariable_in = 'drainagearea')   
+
+        sFilename = os.path.join(  oPyhexwatershed.sWorkspace_output_hexwatershed, 'surface_elevation' )
+        oPyhexwatershed._plot(iFlag_type_in =2, sVariable_in = 'flow_direction') 
+
+        sFilename = os.path.join(  oPyhexwatershed.sWorkspace_output_hexwatershed, 'surface_elevation' )
+        oPyhexwatershed._plot(iFlag_type_in =3, sVariable_in = 'flow_direction')    
+
+        sFilename = os.path.join(  oPyhexwatershed.sWorkspace_output_hexwatershed, 'surface_elevation' )
+        oPyhexwatershed._plot(iFlag_type_in =4, sVariable_in = 'flow_direction')        
+ 
+    sLine  = 'cd ' + oPyhexwatershed.sWorkspace_output + '\n'
+    ofs.write(sLine)
+    sLine  = 'sbatch submit.job' + '\n'
+    ofs.write(sLine)
+
+    iCase_index = iCase_index + 1
+            
+              
+
+ofs.close()
+os.chmod(sFilename, stat.S_IREAD | stat.S_IWRITE | stat.S_IXUSR)   
---
+++ examples/susquehanna/run_simulation_mpas.py
@@ -0,0 +1,97 @@
+
+import os, sys, stat
+from pathlib import Path
+from os.path import realpath
+
+from pyhexwatershed.pyhexwatershed_read_model_configuration_file import pyhexwatershed_read_model_configuration_file
+
+#===========================
+#setup workspace path
+#===========================
+sPath_parent = str(Path(__file__).parents[2]) # data is located two dir's up
+sPath_data = realpath( sPath_parent +  '/data/susquehanna' )
+sWorkspace_input =  str(Path(sPath_data)  /  'input')
+sWorkspace_output = '/compyfs/liao313/04model/pyhexwatershed/susquehanna'
+
+#===================================
+#you need to update this file based on your own case study
+#===================================
+sFilename_configuration_in = realpath( sPath_parent +  '/examples/susquehanna/pyhexwatershed_susquehanna_mpas.json' )
+if os.path.isfile(sFilename_configuration_in):
+    pass
+else:
+    print('This configuration does not exist: ', sFilename_configuration_in )
+
+
+#===========================
+#setup case information
+#===========================
+iFlag_visualization =0 
+iCase_index = 1
+sMesh_type = 'mpas'
+sDate='20220801'
+
+#===================================
+#setup output and HPC job 
+#===================================
+sSlurm = 'short'
+sFilename = sWorkspace_output + '/' + sMesh_type + '.bash'
+ofs = open(sFilename, 'w')
+sLine  = '#!/bin/bash' + '\n'
+ofs.write(sLine)
+
+#===================================
+#visualization spatial extent
+#===================================
+aExtent_full = [-78.5,-75.5, 39.2,42.5]
+aExtent_meander = [-76.5,-76.2, 41.6,41.9] #meander
+aExtent_braided = [-77.3,-76.5, 40.2,41.0] #braided
+aExtent_confluence = [-77.3,-76.5, 40.2,41.0] #confluence
+aExtent_outlet = [-76.0,-76.5, 39.5,40.0] #outlet
+aExtent_dam = [-75.75,-76.15, 42.1,42.5] #dam  
+
+    
+dResolution_meter = 5000
+iFlag_use_mesh_dem = 1
+iFlag_elevation_profile = 1
+#===================================
+#topology turn off
+#===================================
+iFlag_stream_burning_topology = 0
+
+oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
+    iCase_index_in=iCase_index,\
+        iFlag_stream_burning_topology_in=iFlag_stream_burning_topology,\
+            iFlag_use_mesh_dem_in=iFlag_use_mesh_dem,\
+                iFlag_elevation_profile_in=iFlag_elevation_profile,\
+         dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
+oPyhexwatershed.create_hpc_job()            
+
+sLine  = 'cd ' + oPyhexwatershed.sWorkspace_output + '\n'
+ofs.write(sLine)
+sLine  = 'sbatch submit.job' + '\n'
+ofs.write(sLine)
+
+iCase_index = iCase_index + 1
+#===================================
+#topology turn on
+#===================================
+iFlag_stream_burning_topology = 1
+oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
+    iCase_index_in=iCase_index,  iFlag_stream_burning_topology_in=iFlag_stream_burning_topology,\
+          iFlag_use_mesh_dem_in=iFlag_use_mesh_dem,\
+                iFlag_elevation_profile_in=iFlag_elevation_profile,\
+        dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
+
+oPyhexwatershed.create_hpc_job()            
+
+sLine  = 'cd ' + oPyhexwatershed.sWorkspace_output + '\n'
+ofs.write(sLine)
+sLine  = 'sbatch submit.job' + '\n'
+ofs.write(sLine)
+iCase_index = iCase_index + 1
+          
+              
+
+ofs.close()
+os.chmod(sFilename, stat.S_IREAD | stat.S_IWRITE | stat.S_IXUSR)   
